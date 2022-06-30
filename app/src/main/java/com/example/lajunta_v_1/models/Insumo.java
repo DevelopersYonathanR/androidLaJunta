0997b88a529c558c36ee1c7c1cc00be40a66e8c2
YonathanRios2022 <yonathanalejandrorios@gmail.com>
2022-06-29 21:50:45 -0400

    commit

--- app/src/main/java/com/example/lajunta_v_1/models/Insumo.java
+++ app/src/main/java/com/example/lajunta_v_1/models/Insumo.java
@@ -6,4 +6,42 @@ public class Insumo {
     private int cantidad;
     private String medida;
 
+    public Insumo(int id, String nombre, int cantidad, String medida) {
+        this.id = id;
+        this.nombre = nombre;
+        this.cantidad = cantidad;
+        this.medida = medida;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public String getMedida() {
+        return medida;
+    }
+
+    public void setMedida(String medida) {
+        this.medida = medida;
+    }
 }
